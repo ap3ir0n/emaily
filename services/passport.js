cc4e7096fb9bd1b07442ba686daceb1f319520d8
Patrick Luca Fazzi <patrick91@live.it>
2018-02-21 01:05:14 +0100

    Serializzaione utenti

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,13 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
 
 passport.use(
   new GoogleStrategy({
@@ -9,7 +16,14 @@ passport.use(
     callbackURL: '/auth/google/callback'
   },
   (accessToken, refreshToken, profile, done) => {
-    console.log('access token', accessToken);
-    console.log('refresh token', refreshToken);
+    User.findOne({   googleId: profile.id }).then((existingUser) => {
+      if (existingUser) {
+        done(null, existingUser);
+      } else {
+        new User({ googleId: profile.id })
+          .save()
+          .then(user => done(null, user));
+      }
+    });
   })
 );
