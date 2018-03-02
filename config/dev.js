051863b09af80ee718203717916434b2d6b831bd
Ap3ir0n <patrick91@live.it>
2018-03-02 14:44:28 +0100

    Removed keys

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    googleClientID: '446414773577-8jtnbih73mjldf4m9ladquvbi62b8let.apps.googleusercontent.com',
-    googleClientSecret: '9Y56FSDxkjC1pmj1JyEddvwb',
-    mongoURI: 'mongodb://devel:devel@ds149479.mlab.com:49479/emaily-dev',
-    cookieKey: 'asdfghasdsdsajdsakjdkljdklsjadklsajlk'
-};
